a3c33509c25742adc6945f16a58a9f20bba237b3
lepakneo <lepakneo@DESKTOP-HL5TR8U.gateway.2wire.net>
2018-06-20 13:14:07 +0800

    - update Gulp file - Added npm modules

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,87 @@
  */
 
 var gulp = require('gulp');
+var rename = require("gulp-rename");
+const babel = require('gulp-babel');
+const del = require('del'); 
 
-gulp.task('default', function () {
-    // place code for your default task here
+gulp.task('clean',function(){
+   return del(['patch', 'export'])
 });
+
+gulp.task('export1', function () {
+    return gulp.src(
+            ['public_html/**/*.*'
+                        , '!public_html/js/**/*.*'
+                        , '!public_html/kvn/backend/**/*.*'
+                        , '!public_html/kvn/images/bkgd/**/*.*'
+                        , '!public_html/kvn/images/char/**/*.*'
+                        , '!public_html/kvn/scripts/**/*.*'
+                        , '!public_html/kvn/sound/**/*.*'
+            ])
+            .pipe(gulp.dest(['export']));
+});
+
+gulp.task('export2', function () {
+    var a = new Promise(function (resolve) {
+        gulp.src(['public_html/js/**/*.*']).pipe(babel({
+            presets: ['env']
+        })).pipe(gulp.dest(['export/js'])).on('end', function () {
+            resolve();
+        });;
+    });
+    var b = new Promise(function(resolve){
+        gulp.src(['public_html/kvn/backend/**/*.*']).pipe(babel({
+        presets: ['env']
+    })).pipe(gulp.dest(['export/kvn/backend'])).on('end', function () {
+            resolve();
+        });;
+    });
+    var c = new Promise(function(resolve){
+        gulp.src(['public_html/kvn/scripts/di.js'])
+            .pipe(rename(function (path) {
+                path.basename = "init";
+            }))
+            .pipe(gulp.dest(['export/kvn/scripts'])).on('end', function () {
+            resolve();
+        });;
+    });
+    var d = new Promise(function(resolve){
+       gulp.src(['*.*'],{read:false})
+               .pipe(gulp.dest('export/kvn/sound'))
+               .pipe(gulp.dest('export/kvn/images/char'))
+               .pipe(gulp.dest('export/kvn/images/bkgd'));
+    });
+    
+    return Promise.all([a, b, c]);
+});
+
+gulp.task('patch', function () {
+    var a = new Promise(function (resolve) {
+
+        gulp.src(
+                ['export/css/**/*.*'
+                            , '!export/css/config.css'
+                ]).pipe(gulp.dest(['patch/css'])).on('end', function () {
+            resolve();
+        });
+        ;
+    });
+    var b = new Promise(function (resolve) {
+        gulp.src(
+                ['export/kvn/**/*.*'
+                            , '!export/kvn/images'
+                            , '!export/kvn/scripts'
+                            , '!export/kvn/sound'
+                            , '!export/kvn/config.js'])
+                .pipe(gulp.dest(['patch/kvn'])).on('end', function () {
+            resolve();
+        });
+    })
+
+    return Promise.all([a, b]);
+});
+
+
+gulp.task('default', gulp.series('clean','export1', 'export2', 'patch'));
+
